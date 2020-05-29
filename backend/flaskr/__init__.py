9a6e6d9ec3f5e970fb5a887e2ee5e2dca99d6bdc
dubiZA <iamdzh@outlook.com>
2020-05-29 05:44:27 -0600

    Update view endpoints, add questions endpoint test

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,17 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -67,17 +78,17 @@ def create_app(test_config=None):
         }), 500
 
 
-    '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
+#     '''
+#   @TODO: 
+#   Create an endpoint to handle GET requests 
+#   for all available categories.
+#   '''
 
     @app.route('/api/v1/categories')
     def get_categories():
         try:
             categories = Category.query.all()
-            formatted_categories = [category.format() for category in categories]
+            formatted_categories = [category.format()['type'] for category in categories]
 
             return jsonify({
                 'success': True,
@@ -98,6 +109,11 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+    @app.route('/api/v1/questions')
+    def get_questions():
+        pass
+
+
     '''
   @TODO: 
   Create an endpoint to DELETE question using a question ID. 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -36,10 +36,28 @@ class TriviaTestCase(unittest.TestCase):
     def test_get_all_categories(self):
         response = self.client().get('/api/v1/categories')
         data = json.loads(response.data)
-        
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['categories'], True)
+
+    def test_get_paginated_questions(self):
+        response = self.client().get('/api/v1/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_get_paginated_questions_invalid_page(self):
+        response = self.client().get('/api/v1/questions?page=1000')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
+
 
 
 # Make the tests conveniently executable
